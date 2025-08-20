addf22119f0dce9d0ba57a3a60388fb8400081ba
passionryu <rsy1225@naver.com>
2025-08-20 17:49:29 +0900

    feat: Add my local code for new feature X

---
+++ src/test/java/poc_rsy/master_slave/MasterSlaveApplicationTests.java
@@ -0,0 +1,13 @@
+package poc_rsy.master_slave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterSlaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
